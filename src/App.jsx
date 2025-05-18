d99f491e65d6809765cfb689998dac63ca290cdb
Arek <arek93r@gmail.com>
2025-05-18 10:31:35 +0200

    added static calculator UI in Calculator component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div>
+      <h1>Kalkulator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
